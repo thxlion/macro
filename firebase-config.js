d1be15547c3ceb4fbd98a7ae5fe975e6f49bac18
thxlion <zvkarry@gmail.com>
2025-10-15 02:03:23 +0100

    key-fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-window.__FIREBASE_CONFIG__ = {
-  apiKey: "AIzaSyBWTK_ZslSGffYoajYTWPYdpYT0LJzE6cU",
-  authDomain: "macro2-1ad54.firebaseapp.com",
-  projectId: "macro2-1ad54",
-  storageBucket: "macro2-1ad54.firebasestorage.app",
-  messagingSenderId: "894077057303",
-  appId: "1:894077057303:web:826c217a46798015125f52",
-  measurementId: "G-G79X63WY5R"
-};
+window.__FIREBASE_CONFIG__ = window.__FIREBASE_CONFIG__ || null;
